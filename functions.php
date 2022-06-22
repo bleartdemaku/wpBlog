4d1926afb7ede18be3f035067acea620ec119360
bleartdemaku <bd47165@ubt-uni.net>
2022-06-22 23:56:11 +0200

    fixed some bugs and added loop for posts

--- functions.php
+++ functions.php
@@ -1,21 +1,16 @@
-<?php
+<?php 
 
-//including stylesheet and script files
-
-function load_scripts() {
-    wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
-
-    wp_enqueue_script( 'bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js', array( 'jquery' ), '5.0.2', true );
-
-    wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css', array(), '5.0.2', 'all' );
-
+// Including stylesheet and script files
+function load_scripts(){
+	wp_enqueue_script( 'bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js', array( 'jquery' ), '4.0.0', true );
+	wp_enqueue_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css', array(), '4.0.0', 'all' );	
+	wp_enqueue_style( 'template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'load_scripts' );
 
 register_nav_menus(
-    array(
-        'my_main_menu' => 'Main Menu',
-        'footer_menu' => 'Footer Menu'
-    )
+	array(
+		'my_main_menu' => 'Main Menu',
+		'footer_menu' => 'Footer Menu'
+	)
 );
-
