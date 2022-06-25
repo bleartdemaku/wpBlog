15cc702297707a946695c74e6b16db06d38a5849
bleartdemaku <bd47165@ubt-uni.net>
2022-06-25 17:49:44 +0200

    added posts and posts formats

--- functions.php
+++ functions.php
@@ -26,6 +26,7 @@ function learnwp_config(){
 	);
 	add_theme_support( 'custom-header', $args );
 	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-formats', array( 'video', 'image' ) );
 
 }
 add_action( 'after_setup_theme', 'learnwp_config', 0 );
--- index.php
+++ index.php
@@ -2,47 +2,50 @@
 
 <img class="img-fluid" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
 
-<div class="content-area">
-	<main>
-		<section class="slide">
-			<div class="container">
-				<div class="row">Slide</div>
-			</div>
-		</section>
-		<section class="services">
-			<div class="container">
-				<div class="row">Services</div>
-			</div>
-		</section>
-		<section class="middle-area">
-			<div class="container">
-				<div class="row">
-					<aside class="sidebar col-md-3">Sidebar</aside>
-					<div class="news col-md-9">
-						<?php
+	<div class="content-area">
+		<main>
+			<section class="slide">
+				<div class="container">
+					<div class="row">Slide</div>
+				</div>
+			</section>
+			<section class="services">
+				<div class="container">
+					<div class="row">Services</div>
+				</div>				
+			</section>
+			<section class="middle-area">
+				<div class="container">
+					<div class="row">
+						<aside class="sidebar col-md-3">Sidebar</aside>
+						<div class="news col-md-9">
+							<?php 
 
-						// If there are any posts
-						if (have_posts()) :
-							// While have posts, show them to us
-							while (have_posts()) : the_post();
-								get_template_part('template-parts/content');
-							endwhile;
-						else :
-							?>
+							// If there are any posts
+							if( have_posts() ):
+								// While have posts, show them to us
+								while( have_posts() ): the_post();
 
-							<p>There's nothing yet to be displayed!</p>
+								// Require the file which is at template-parts/content.php
+								get_template_part( 'template-parts/content', get_post_format() );
 
-						<?php endif; ?>
+							 	endwhile;
+							 else: 
+							  ?>
 
+							  <p>There's nothing yet to be displayed!</p>
+
+							<?php endif; ?>
+
+						</div>							
 					</div>
 				</div>
-			</div>
-		</section>
-		<section class="map">
-			<div class="container">
-				<div class="row">Map</div>
-			</div>
-		</section>
-	</main>
-</div>
+			</section>
+			<section class="map">
+				<div class="container">
+					<div class="row">Map</div>
+				</div>				
+			</section>
+		</main>
+	</div>
 <?php get_footer(); ?>
