a9612c7204fd135e8c4cf3421aba02b050579a09
bleartdemaku <bd47165@ubt-uni.net>
2022-07-01 11:47:04 +0200

    added wp_query for multiple loops in one page

--- page-home.php
+++ page-home.php
@@ -60,7 +60,22 @@
 								
 						</aside>
 						<div class="news col-md-9">
-						<p>This will be the news area of the Home Page</p>
+						<div class="row">
+							<div class="container">
+								<?php 
+
+								$featured = new WP_Query( 'post_type=post&posts_per_page=1&cat=4,11' );
+
+								if( $featured->have_posts() ):
+									while( $featured->have_posts() ): $featured->the_post();
+									endwhile;
+									wp_reset_postdata();
+								endif;
+
+
+								?>
+							</div>
+						</div>
 
 						</div>							
 					</div>
