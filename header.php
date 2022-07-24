296e6a22bcfbbdbcda22f34ff8f9d43071275d47
bleartdemaku <bd47165@ubt-uni.net>
2022-07-24 21:56:36 +0200

    added archive & 404 page + template name plugin

--- header.php
+++ header.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
+
 <head>
-	<meta charset="UTF-8">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Learn WP</title>
+	<title><?php wp_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
 	<header>
 		<section class="top-bar">
 			<div class="container">
 				<div class="row">
 					<div class="social-media-icons col-xl-9 col-md-7 col-sm-7 col-6">Social Icons</div>
-					<div class="search col-xl-3 col-md-5 col-sm-5 col-6 text-right"><?php get_search_form(); ?></div>					
+					<div class="search col-xl-3 col-md-5 col-sm-5 col-6 text-right"><?php get_search_form(); ?></div>
 				</div>
 			</div>
 
@@ -23,10 +25,10 @@
 					<div class="row">
 						<section class="logo col-md-2 col-sm-12 col-12 text-center">Logo</section>
 						<nav class="main-menu col-md-10 text-right">
-							<?php wp_nav_menu( array( 'theme_location' => 'my_main_menu' ) ); ?>
-						</nav>					
-					</div>					
+							<?php wp_nav_menu(array('theme_location' => 'my_main_menu')); ?>
+						</nav>
+					</div>
 				</div>
-			</div>				
+			</div>
 		</section>
 	</header>
