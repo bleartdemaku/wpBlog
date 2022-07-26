e77d7470fdafe3b8b54ee76922554cdfcac98b9e
bleartdemaku <bd47165@ubt-uni.net>
2022-07-26 13:14:39 +0200

    added social icons plugin and widget+css

--- functions.php
+++ functions.php
@@ -89,5 +89,16 @@ function learnwp_sidebars(){
 			'before_title' => '<h2 class="widget-title">',
 			'after_title' => '</h2>'
 		)
-	);							
+	);			
+	register_sidebar(
+		array(
+			'name' => 'Social Media Icons',
+			'id' => 'social-media',
+			'description' => 'Social Media Icons Widgets. Drag and drop your widgets here. ',
+			'before_widget' => '<div class="widget-wrapper">',
+			'after_widget' => '</div>',
+			'before_title' => '<h2 class="widget-title">',
+			'after_title' => '</h2>'
+		)
+	);						
 }
